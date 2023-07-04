c86bd07f3e06f0040acee9e2fcd2a010896630bb
sferez <simeon.ferez@gmail.com>
2023-07-04 13:16:16 +0200

    feat: global vocab

--- src/entropyEstimation/run-dir.py
+++ src/entropyEstimation/run-dir.py
@@ -34,11 +34,15 @@ def main():
 
                 output_dir = os.path.join(dir.split("/")[-1], file_no_ext)
 
+                if global_vocab:
+                    vocab = global_vocab
+
                 print(filename)
                 print(tokens)
                 print(vocab)
                 print(output_dir)
-                os.system(f'python3 src/entropyEstimation/full-analysis.py --t {tokens} --v {vocab} --o {output_dir} {"--f" if fast else ""}')
+                os.system(
+                    f'python3 src/entropyEstimation/full-analysis.py --t {tokens} --v {vocab} --o {output_dir} {"--f" if fast else ""} {"--uv" if global_vocab else ""}')
                 print("------------------------------------------------------------")
 
 
@@ -48,9 +52,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run analysis on a directory of files.")
     parser.add_argument('--input_dir', '--i', type=str, help="Directory containing files to analyze.")
     parser.add_argument('--fast', '--f', action=argparse.BooleanOptionalAction, default=False, help='Fast mode')
+    parser.add_argument('--vocab', '--v', type=str,
+                        help='Path to vocabulary, if using a global one for the whole directory', default=None,
+                        required=False)
 
     args = parser.parse_args()
     dir = args.input_dir
     fast = args.fast
+    global_vocab = args.vocab
 
     main()
